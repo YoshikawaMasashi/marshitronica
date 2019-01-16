115f10e7b3bf29da718ad5e5c27d158adf78d8cf
masashi yoshikawa <yoshikawa@al4.biz>
2019-01-16 19:55:33 +0900

    [add] start.scd test.py

---
+++ test.py
@@ -0,0 +1,38 @@
+from pythonosc import osc_message_builder
+from pythonosc import udp_client
+from pythonosc import osc_bundle_builder
+
+
+client = udp_client.SimpleUDPClient('localhost', 57110)
+
+msg = osc_message_builder.OscMessageBuilder(address='/s_new')
+msg.add_arg("test_osc", arg_type='s')
+msg.add_arg(1000, arg_type='i')
+msg.add_arg(1, arg_type='i')
+msg.add_arg(1, arg_type='i')
+msg = msg.build()
+client.send(msg)
+
+msg = osc_message_builder.OscMessageBuilder(address='/n_free')
+msg.add_arg(1000, arg_type='i')
+msg = msg.build()
+client.send(msg)
+
+bundle = osc_bundle_builder.OscBundleBuilder(
+    osc_bundle_builder.IMMEDIATELY)
+msg = osc_message_builder.OscMessageBuilder(address='/s_new')
+msg.add_arg("test_osc", arg_type='s')
+msg.add_arg(1000, arg_type='i')
+msg.add_arg(1, arg_type='i')
+msg.add_arg(1, arg_type='i')
+bundle.add_content(msg.build())
+bundle = bundle.build()
+client.send(bundle)
+
+bundle = osc_bundle_builder.OscBundleBuilder(
+    osc_bundle_builder.IMMEDIATELY)
+msg = osc_message_builder.OscMessageBuilder(address='/n_free')
+msg.add_arg(1000, arg_type='i')
+bundle.add_content(msg.build())
+bundle = bundle.build()
+client.send(bundle)
