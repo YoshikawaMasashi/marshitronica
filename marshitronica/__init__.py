9783bdeb90f1e2ca9bdd592f07476369ea6702bd
masashi yoshikawa <yoshikawa@al4.biz>
2019-01-18 20:59:59 +0900

    [add] scheduler

---
+++ marshitronica/__init__.py
@@ -0,0 +1,1 @@
+from marshitronica.scheduler import Scheduler  # NOQA
---
+++ marshitronica/scheduler.py
@@ -0,0 +1,29 @@
+import time
+import threading
+
+
+class Scheduler(object):
+    """Scheduler
+    """
+    def __init__(self):
+        self.is_running = False
+        self.start_time = None
+        self.step_second = 0.0001
+
+    def __run(self):
+        while self.is_running:
+            time.sleep(self.step_second)
+
+    def start(self):
+        """Start
+        """
+        # TODO: if started, raise Exception
+        self.is_running = True
+        self.start_time = time.time()
+        threading.Thread(None, self.__run)
+
+    def stop(self):
+        """Stop
+        """
+        self.is_running = False
+        self.start_time = None
