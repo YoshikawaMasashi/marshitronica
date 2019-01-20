1af2f20b8bb4e2ef05869c124cb9c07c31e3f6c6
masashi yoshikawa <yoshikawa@al4.biz>
2019-01-20 13:20:06 +0900

    [add] load synthdef

--- marshitronica/__init__.py
+++ marshitronica/__init__.py
@@ -1,1 +1,20 @@
+import glob
+import os
+import pathlib
+
+from pythonosc import osc_message_builder
+from pythonosc import udp_client
+
 from marshitronica.scheduler import Scheduler  # NOQA
+
+
+# load synthdef
+module_path = pathlib.Path(os.path.dirname(__file__))
+
+client = udp_client.SimpleUDPClient('localhost', 57110)
+
+for path in glob.glob(str(module_path / 'scd/*.scd')):
+    msg = osc_message_builder.OscMessageBuilder(address='/d_load')
+    msg.add_arg(path, arg_type='s')
+    msg = msg.build()
+    client.send(msg)
