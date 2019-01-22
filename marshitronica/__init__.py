9796eee149ca2748144b4431c087e6ae7beccf0c
masashi yoshikawa <yoshikawa@al4.biz>
2019-01-23 08:02:09 +0900

    [fix] scsyndef

--- marshitronica/__init__.py
+++ marshitronica/__init__.py
@@ -16,7 +16,7 @@ module_path = pathlib.Path(os.path.dirname(__file__))
 
 client = udp_client.SimpleUDPClient('localhost', 57110)
 
-for path in glob.glob(str(module_path / 'scd/*.scd')):
+for path in glob.glob(str(module_path / 'scsyndef/*.scsyndef')):
     msg = osc_message_builder.OscMessageBuilder(address='/d_load')
     msg.add_arg(path, arg_type='s')
     msg = msg.build()
