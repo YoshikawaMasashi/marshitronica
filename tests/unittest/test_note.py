ce838b03df8f009174a406fd177680fd28297717
masashi yoshikawa <yoshikawa@al4.biz>
2019-01-26 14:33:30 +0900

    [add] test_note

---
+++ tests/unittest/test_note.py
@@ -0,0 +1,19 @@
+import unittest
+
+import marshitronica as mt
+
+
+class TestNote(unittest.TestCase):
+
+    def setUp(self):
+        super(TestNote, self).setUp()
+        self.note = mt.cpplib.Note(60, 1, 0.5)
+
+    def test_get_pitch(self):
+        self.assertEqual(60, self.note.get_pitch())
+
+    def test_get_duration(self):
+        self.assertEqual(1, self.note.get_duration())
+
+    def test_get_amp(self):
+        self.assertEqual(0.5, self.note.get_amp())
