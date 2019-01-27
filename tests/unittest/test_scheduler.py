eabe7074840113421c2029bff1c8425159aef72b
masashi yoshikawa <yoshikawa@al4.biz>
2019-01-27 20:56:07 +0900

    [add] increment test for scheduler

--- tests/unittest/test_scheduler.py
+++ tests/unittest/test_scheduler.py
@@ -1,16 +1,36 @@
 import unittest
+import time
 
 import marshitronica as mt
+
+
+class Incrementer(object):
+    def __init__(self):
+        self.x = 0
+
+    def __call__(self):
+        self.x += 1
 
 
 class TestScheduler(unittest.TestCase):
 
     def setUp(self):
         super(TestScheduler, self).setUp()
-        self.scheduler = mt.Scheduler()
 
     def test_seconds_to_beats(self):
-        self.assertEqual(2, self.scheduler.seconds_to_beats(1))
+        scheduler = mt.Scheduler()
+        self.assertEqual(2, scheduler.seconds_to_beats(1))
 
     def test_beats_to_seconds(self):
-        self.assertEqual(0.5, self.scheduler.beats_to_seconds(1))
+        scheduler = mt.Scheduler()
+        self.assertEqual(0.5, scheduler.beats_to_seconds(1))
+
+    def test_scheduling(self):
+        scheduler = mt.Scheduler()
+        scheduler.start()
+        incrementer = Incrementer()
+        scheduler.add_task(1, incrementer)
+        time.sleep(0.4)
+        self.assertEqual(0, incrementer.x)
+        time.sleep(0.2)
+        self.assertEqual(1, incrementer.x)
