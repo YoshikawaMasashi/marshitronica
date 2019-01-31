afe42f0e280c1109490996b4e0104a0f21a7ff6c
masashi yoshikawa <yoshikawa@al4.biz>
2019-01-31 22:09:10 +0900

    load tsf

--- src/sound_font.cpp
+++ src/sound_font.cpp
@@ -1,6 +1,13 @@
 // Copyright[2019] <marshi(masashi yoshikawa)>
+#define TSF_IMPLEMENTATION
 #include "./sound_font.h"
 
+
 SoundFont::SoundFont(std::string filename) {
-  
+  this->tsf_struct = tsf_load_filename(filename.c_str());
+  std::cout << tsf_struct->presets[0].presetName << std::endl;
+}
+
+SoundFont::~SoundFont() {
+  TSF_FREE(this->tsf_struct);
 }
--- src/sound_font.h
+++ src/sound_font.h
@@ -2,9 +2,13 @@
 #pragma once
 
 #include <string>
+#include <iostream>
+#include "./tsf.h"
 
 
 class SoundFont {
  public:
   SoundFont(std::string);
+  ~SoundFont();
+  tsf* tsf_struct;
 };
